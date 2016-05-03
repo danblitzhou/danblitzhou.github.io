33ba0cab9ddd01696ec02a6307128af4775a233e
danblitzhou <danzhou1810@gmail.com>
2016-05-03 19:28:17 -0400

    home sweet home

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 angular.module('ViewSwitchApp', ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-  .when('/', {templateUrl: 'templates/home.html'})
+  .when('/home', {templateUrl: 'templates/home.html'})
   .when('/education', {templateUrl: 'templates/education.html'})
   .when('/skills', {templateUrl: 'templates/skills.html'})
   .when('/contact', {templateUrl: 'templates/contact.html'})
