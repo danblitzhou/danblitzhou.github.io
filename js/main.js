5db1a45655c48b3182c592102dc1b482decfcd4a
danblitzhou <danzhou1810@gmail.com>
2016-05-14 16:30:09 -0400

    Added controller for projects page; created test modal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 angular.module('ViewSwitchApp', ['ngRoute'])
+.controller('ProjectsController', function($scope, $routeParams) {
+  $(document).ready(function() {
+    $('.modal-trigger').leanModal();
+  });
+})
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
   .when('/home', {templateUrl: 'templates/home.html'})
   .when('/experience', {templateUrl: 'templates/experience.html'})
   .when('/skills', {templateUrl: 'templates/skills.html'})
-  .when('/projects', {templateUrl: 'templates/projects.html'})
+  .when('/projects', {
+    templateUrl: 'templates/projects.html',
+    controller: 'ProjectsController'
+})
   .otherwise({redirectTo: '/home'});
 }]);
